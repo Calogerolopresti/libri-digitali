400a30b8936d5584f1f5cbfcd6a82050a6c6557c
Il Tuo Nome <la-tua-email@esempio.com>
2026-05-10 15:28:58 +0200

    Aggiunta gestione della selezione dei prodotti con gestione degli errori

--- includes/select_prodotti.php
+++ includes/select_prodotti.php
@@ -5,4 +5,16 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
+
+try{
+    // scrivo la select per mostrarmi tutti i prodotti 
+    $sql = 'SELECT * FROM Prodotti';
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    $libri = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch(PDOException $e) {
+   $libri = [];
+}
 ?>
