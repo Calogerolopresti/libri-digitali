65498e0b8cd51e107ea464a48b0daec33559ca82
Calogerolopresti <calogerolopresti2005@gmail.com>
2026-05-15 15:47:13 +0200

    Aggiunta di controlli di accesso e gestione della sessione nella modifica dei prodotti

--- admin/modifica.php
+++ admin/modifica.php
@@ -1,5 +1,24 @@
 <?php
-require_once '../config/db.php';
-// Logica per modificare il prodotto (gestire $_POST e update)
-header('Location: index.php');
-exit;
+// importo collegamento al db e avvio la sessione se non è già avviata 
+require_once __DIR__ . '/../config/db.php';
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// controllo se l utente è con ruolo user o se ha mai fatto accesso e se non lo è lo butto fuori 
+if (!isset($_SESSION['user_id']) || $_SESSION['ruolo'] !== 'admin') {
+    header('Location:../index.php');
+    exit();
+}
+// aggiungere controllo token csrf 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    try{
+        $sql="UPDATE Prodotti SET titolo=?,descrizione=?,prezzo=?,formato=?,disponibilita=?,copertina=? WHERE id=?";
+        $stmt=$pdo->prepare($sql);
+        $stmt->execute([$_POST['titolo'],$_POST['descrizione'],$_POST['prezzo'],$_POST['formato'],$_POST['quantita'],$_POST['copertina'],$_POST['id_prodotto']]);
+        header('Location:index.php');
+    }catch(PDOException $e){
+        echo $e;
+    }
+}
+?>
